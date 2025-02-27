0d9e8e5db5edc58ef3ea9f98d0928e04b17a032a
Alberto Quiles Boix <alberto.quil3s@gmail.com>
2025-02-27 17:43:35 +0100

    bump: update @oh/utils + getApiHandler - fix #44 (#45)

--- src/modules/api/v1/teleports/link.request.ts
+++ src/modules/api/v1/teleports/link.request.ts
@@ -3,23 +3,18 @@ import {
   HttpStatusCode,
   RequestMethod,
   RequestType,
+  RequestKind,
 } from "@oh/utils";
-import {
-  getHotelFromRequest,
-  isRequestAuthenticated,
-} from "shared/utils/request.utils.ts";
+import { getHotelFromRequest } from "shared/utils/request.utils.ts";
 import { System } from "modules/system/main.ts";
 import { Scope } from "shared/enums/scope.enums.ts";
 
 export const linkRequest: RequestType<unknown> = {
   method: RequestMethod.POST,
   pathname: "/link",
+  kind: RequestKind.TOKEN,
   func: async (request) => {
-    if (!isRequestAuthenticated(request))
-      return getResponse(HttpStatusCode.FORBIDDEN);
-
     const { accountId, linkId, teleportId } = await request.json();
-
     if (!accountId || !linkId || !teleportId)
       return getResponse(HttpStatusCode.FORBIDDEN);
 
--- src/shared/utils/request.utils.ts
+++ src/shared/utils/request.utils.ts
@@ -1,16 +1,5 @@
 import { System } from "modules/system/main.ts";
 import { HotelMutable } from "shared/types/hotel.types.ts";
-import { getResponse, HttpStatusCode } from "@oh/utils";
-
-export const isRequestAuthenticated = (request: Request) => {
-  const hotelId = request.headers.get("hotel-id");
-  const token = request.headers.get("token");
-
-  if (!hotelId || !token) return getResponse(HttpStatusCode.FORBIDDEN);
-
-  const hotel = System.hotels.get({ hotelId });
-  return hotel.verify(token);
-};
 
 export const getHotelFromRequest = (request: Request): HotelMutable => {
   const hotelId = request.headers.get("hotel-id");
